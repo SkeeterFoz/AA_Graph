91bff259d79c8b6f0d88ef2d9a8999df1a639a83
Newton <skeeterfoz@gmail.com>
2009-10-03 01:51:57 -0300

    Operação isOrientado

--- src/controller/Operacoes.java
+++ src/controller/Operacoes.java
@@ -4,8 +4,19 @@ import model.Graph;
 
 public class Operacoes {
 
-	public boolean isOrientado(Graph grafo) {
-		return true;
+	public static boolean isOrientado(Graph grafo) {
+		byte orientado = 0;
+		
+		for (int i = 0; i < grafo.getNlc(); i++) {
+			for (int j = 0; j < grafo.getNlc(); j++) {
+				if (grafo.getElement(i, j) != grafo.getElement(j, i)) {
+					orientado = 1;
+				}
+			}
+		}
+		if (orientado == 1) {
+			return true;
+		} else return false;
 	}
 	
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -4,17 +4,25 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import model.Graph;
 
 import controller.IOFile;
+import controller.Operacoes;
 import controller.Parser;
 
 
 public class Main {
 
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		Graph grafo;
+		
 		JFileChooser fc = new JFileChooser(".");
 		int returnVal = fc.showOpenDialog(null);
 		
@@ -25,7 +33,14 @@ public class Main {
 			io.Read();
 			
 			Parser parser = new Parser(io.getV(), io.getN());
-			parser.getGrafo().printGraph();
+			grafo = parser.getGrafo();
+			grafo.printGraph();
+			
+			if (Operacoes.isOrientado(grafo)) {
+				JOptionPane.showMessageDialog(null, "O grafo é Orientado", "Tipo de Grafo", JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "O grafo não é Orientado", "Tipo de Grafo", JOptionPane.INFORMATION_MESSAGE);
+			}
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
