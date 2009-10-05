514a63483e18b5c1225f42237f4e6f886812144a
Igor Henrique da Cruz <spym4n@gmail.com>
2009-10-05 10:26:41 -0300

    Parte Grafica

--- src/controller/Parser.java
+++ src/controller/Parser.java
@@ -1,97 +1,98 @@
 package controller;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import model.Graph;
 
+/**
+ * @author igor
+ *
+ */
 public class Parser {
 
-	private byte[][] matriz;
-	
 	/**
 	 * Realiza o parser do vetor de strings passado
 	 * @param v Vetor com as strings
 	 * @param nl Numero de linhas do vetor
+	 * @param g Grafo de saida gerado pelo parser
 	 */
-	public Parser(String[] v, int nl) {
-		this.matriz = new byte[nl][nl];
+	public Parser(String[] v, int nl, Graph g) {
+	
+		byte matriz[][]; 
+		matriz = new byte[nl][nl];
 		
-		int i = 0;
+		int i=0;
+
 		while (i < nl) {
-			int c = 0;
-			while (c < nl) {
-				if (v[i].charAt(c) == '1') {
-					this.matriz[i][c] = 1;
+			int j=0;
+			int k=0;
+			while (j < nl) {
+				if (v[i].charAt(k) == ',') {
+					k++;
+					continue;
 				}
+					
+				if (v[i].charAt(k) == '1') {
+					matriz[i][j] = 1;
+				}
+				k++;
+				j++;
 			}
+			i++;
 		}
+	
+		g.setMatriz(matriz);
+		g.setNlc(nl);
 	}
 	
 	public void TextToGraph(String _path, Graph g) throws Exception {
 	}
 	
-//	public void GraphToXml(Graph g, ArrayList<Edge> _pathlist, int _metrica, String _path) throws IOException {
-//		
-//		StringBuffer xml = new StringBuffer(
-//		"<?xml version=\"1.0\" encoding=\"ISO8859-1\"?>\n<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\">\n" + 
-//		"\t<graph edgedefault=\"undirected\">\n" +
-//    	"\t<key id=\"name\" for=\"node\" attr.name=\"name\" attr.type=\"string\"/>\n" +
-//    	"\t<key id=\"state\" for=\"node\" attr.name=\"state\" attr.type=\"string\"/>\n" +
-//    	"\t<key id=\"sa\" for=\"node\" attr.name=\"sa\" attr.type=\"string\"/>\n" +
-//    	"\t<key id=\"weight\" for=\"edge\" attr.name=\"weight\" attr.type=\"string\"/>\n" +
-//    	"\t<key id=\"waycolor\" for=\"edge\" attr.name=\"waycolor\" attr.type=\"string\"/>\n"
-//    	);
-//		
-//		for (int i=0; i < g.getNodelist().size(); i++) {
-//			
-//			xml.append(
-//			"\n\t\t<node id=\"" + i + "\">" +
-//			"\n\t\t\t<data key=\"name\">" + g.getNodelist().get(i).getName() + "</data>" + 
-//            "\n\t\t\t<data key=\"state\">" + ((g.getNodelist().get(i).isState()) ? "true" : "false") + "</data>" + 
-//            "\n\t\t\t<data key=\"sa\">" + g.getNodelist().get(i).getSa() + "</data>" + 
-//            "\n\t\t</node>");
-//		}
-//		
-//		for (int i=0; i < g.getEdgelist().size(); i++) {
-//			xml.append(
-//			"\n\n\t\t<edge source=\"" + g.getEdgelist().get(i).getSrc().getId() + "\" target=\"" + g.getEdgelist().get(i).getDst().getId() + "\">" +
-//			"\n\t\t\t<data key=\"weight\">");
-//			System.out.println("metrica: " + _metrica);
-//			switch (_metrica) {
-//			case 0: xml.append( ((g.getEdgelist().get(i).isEqualsa()) ? g.getEdgelist().get(i).getDist() : g.getEdgelist().get(i).getUserdist())); break;
-//			case 1: xml.append( ((g.getEdgelist().get(i).isEqualsa()) ? g.getEdgelist().get(i).getRettime() : g.getEdgelist().get(i).getUserrettime())); break;
-//			case 2: xml.append( ((g.getEdgelist().get(i).isEqualsa()) ? g.getEdgelist().get(i).getHopcount() : g.getEdgelist().get(i).getUserhopcount())); break;
-//			case 3: xml.append( ((g.getEdgelist().get(i).isEqualsa()) ? g.getEdgelist().get(i).getBandwidth() : g.getEdgelist().get(i).getUserbandwidth())); break;
-//			}
-//			
-//			xml.append("</data>");
-//			xml.append("\n\t\t\t<data key=\"waycolor\">");
-//			if (_pathlist != null) {
-//				if (ConstainsPath(_pathlist, g.getEdgelist().get(i).getSrc().getId(), g.getEdgelist().get(i).getDst().getId())) {
-//					xml.append("color3</data>\n\n\t</edge>");
-//					continue;
-//				}
-//			}
-//			
-//			xml.append((((g.getEdgelist().get(i).getSrc().isState()) && (g.getEdgelist().get(i).getDst().isState())) ? "color1" : "color2") + "</data>");
-//
-//			xml.append("\n\t\t</edge>");
-//		}
-//		
-//		xml.append("\n\n\t</graph>\n</graphml>");
-//		
-//		BufferedWriter out = new BufferedWriter(new FileWriter(_path));
-//        out.write(xml.toString());
-//        out.flush();
-//        out.close();
-//	
-//	}
-//
-//	private boolean ConstainsPath(ArrayList<Edge> path, long src, long dst)
-//	{
-//		for (int i=0; i < path.size(); i++)
-//			if ((path.get(i).getSrc().getId() == src) && (path.get(i).getDst().getId() == dst))
-//				return true;
-//		return false;
-//	}
+	/**
+	 * Cria o arquivo XML para entrada do prefuse
+	 * @param g Grafo de entrada
+	 * @param _path Path do arquivo destino do xml gerado
+	 */
+	public void GraphToXml(Graph g, String _path) throws IOException {
 		
+		StringBuffer xml = new StringBuffer(
+		"<?xml version=\"1.0\" encoding=\"ISO8859-1\"?>\n<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\">\n" + 
+		"\t<graph edgedefault=\"directed\">\n" +
+    	"\t<key id=\"name\" for=\"node\" attr.name=\"name\" attr.type=\"string\"/>\n" +
+    	"\t<key id=\"type\" for=\"node\" attr.name=\"type\" attr.type=\"string\"/>\n" +
+		"\t<key id=\"waycolor\" for=\"edge\" attr.name=\"waycolor\" attr.type=\"string\"/>\n"
+    	);
+		byte mtx[][] = g.getMatriz();
+		
+		for (int i=0; i < g.getNlc(); i++) {
+			xml.append(
+			"\n\t\t<node id=\"" + i + "\">" +
+			"\n\t\t\t<data key=\"name\">" + (i+1) + "</data>" +
+			"\n\t\t\t<data key=\"type\">normal</data>" +
+			"\n\t\t</node>");
+		}
+		
+		
+		for (int i=0; i < g.getNlc(); i++) {
+			for (int j=0; j < g.getNlc(); j++) {
+				if ((mtx[i][j] == 1) || (mtx[i][j] == 2)) {
+					xml.append("\n\n\t\t<edge source=\"" + i + "\" target=\"" + j + "\"> " +
+							"\n\t\t\t<data key=\"waycolor\">color"+((mtx[i][j] == 1) ? 1 : 2)+"</data>\n\t\t</edge>");
+				}
+				
+			}
+		}
+				
+		xml.append("\n\n\t</graph>\n</graphml>");
+		
+		BufferedWriter out = new BufferedWriter(new FileWriter(_path));
+        out.write(xml.toString());
+        out.flush();
+        out.close();
+	
+	}
+	
 }
 
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,37 +1,293 @@
 package view;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import javax.swing.JFileChooser;
-
+import model.Graph;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
 import controller.IOFile;
+import controller.Parser;
 
-// TEste de commit
+/**
+ * @author igor
+ *
+ */
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		JFileChooser fc = new JFileChooser(".");
-		int returnVal = fc.showOpenDialog(null);
-		
-		if(returnVal != JFileChooser.APPROVE_OPTION) return; 
-		
+	private Shell shell;
+	private Menu mainMenu;
+	private MenuItem fileMenuItem;
+	private Menu fileMenu;
+	private MenuItem openFileMenuItem;
+	private MenuItem exitMenuItem;
+	private MenuItem helpMenuItem;
+	private Menu helpMenu;
+	private MenuItem contentsMenuItem;
+	private MenuItem aboutMenuItem;
+	private Composite groupComposite;
+	private Composite graphComposite;
+	
+	private String fileName;
+	private Parser parser;
+	private IOFile io;
+	private Graph graph;
+	
+	private java.awt.Frame graphFrame;
+	private java.awt.Panel panel;
+	
+	private Button btnCiclo;
+	
+	public Main(Composite parent, int style) {
+		this.shell = parent.getShell();
+		initGUI();
+	}
+	
+	
+	private void initGUI() {
 		try {
-			IOFile io = new IOFile(fc.getSelectedFile().getAbsolutePath());
-			io.Read();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
+			mainMenu = new Menu(shell, SWT.BAR);
+			shell.setMenuBar(mainMenu);
+				fileMenuItem = new MenuItem(mainMenu, SWT.CASCADE);
+				fileMenuItem.setText("Arquivo");
+				fileMenu = new Menu(fileMenuItem);
+					openFileMenuItem = new MenuItem(fileMenu, SWT.CASCADE);
+					openFileMenuItem.setText("Abrir");
+					openFileMenuItem.addSelectionListener(new SelectionAdapter() {
+						public void widgetSelected(SelectionEvent evt) {
+							openFile();
+						}
+					});
+
+					exitMenuItem = new MenuItem(fileMenu, SWT.CASCADE);
+					exitMenuItem.setText("Sair");
+					exitMenuItem.addSelectionListener(new SelectionAdapter() {
+						public void widgetSelected(SelectionEvent evt) {
+							shell.dispose();
+						}
+					});
+
+				fileMenuItem.setMenu(fileMenu);	
+				
+				helpMenuItem = new MenuItem(mainMenu, SWT.CASCADE);
+				helpMenuItem.setText("Ajuda");
+					helpMenu = new Menu(helpMenuItem);
+						contentsMenuItem = new MenuItem(helpMenu, SWT.CASCADE);
+						contentsMenuItem.setText("Conteúdo");
+						contentsMenuItem.addSelectionListener(new SelectionAdapter() {
+							public void widgetSelected(SelectionEvent evt) {
+								final Shell dialog = new Shell(shell);
+							    dialog.setText("Conteúdo");
+							    dialog.setSize(600, 150);
+							    dialog.setLayout(new RowLayout(SWT.VERTICAL));
+							    
+							        new StyledText(dialog, SWT.PUSH).setText("Este trabalho foi desenvolvido p/ a disciplina de AA e abrange o escopo da teoria sobre grafos, \n" + 
+							    		"contendo assim a implementação dos algoritmos de verificação de componentes conexas, \n" +
+							    		"verificação da existência de ciclos, verificação de vértices e arestas de corte e \n" +
+							    		"análise de bipartição para os grafos simples. Para dígrafos, os algoritmos implementados \n" +
+							    		"verificação de componentes fortemente conexas, fecho transitivo e ordenação topológica.\n");
+							    Button btnOk = new Button(dialog, SWT.PUSH);
+								btnOk.setText("Ok");
+								btnOk.addListener(SWT.Selection, new Listener() {
+									public void handleEvent(Event event) {
+										dialog.dispose();
+									}
+								});
+								dialog.open();
+							}
+						});
+						
+						aboutMenuItem = new MenuItem(helpMenu, SWT.CASCADE);
+						aboutMenuItem.setText("Sobre");
+						aboutMenuItem.addSelectionListener(new SelectionAdapter() {
+							public void widgetSelected(SelectionEvent evt) {
+								final Shell dialog = new Shell(shell);
+							    dialog.setText("Sobre");
+							    dialog.setSize(350, 150);
+							    dialog.setLayout(new RowLayout(SWT.VERTICAL));
+							    new StyledText(dialog, SWT.PUSH).setText("Implementação da Teoria de Grafos 1.0 \n\n" + 
+							    		"Alunos: \n\t Igor Henrique da Cruz (spym4n@gmail.com)\n\t Newtom Muchael (skeeterfoz@gmail.com)\n");
+							    Button btnOk = new Button(dialog, SWT.PUSH);
+								btnOk.setText("Ok");
+								btnOk.addListener(SWT.Selection, new Listener() {
+									public void handleEvent(Event event) {
+										dialog.dispose();
+									}
+								});
+							    dialog.open();
+							}
+						});
+						
+				helpMenuItem.setMenu(helpMenu);
+
+						
+				
+			groupComposite = new Composite(shell, SWT.NONE);
+			//groupComposite.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_BEGINNING));
+			groupComposite.setLayout(new FillLayout());
+			
+			Composite fakeComposite = new Composite(groupComposite, SWT.BEGINNING);
+			//groupComposite.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_BEGINNING));
+			fakeComposite.setLayout(new RowLayout(SWT.BEGINNING));
+			
+			Group groupGrafo = new Group(fakeComposite, SWT.SHADOW_IN);
+			groupGrafo.setText("        Algoritmos Grafo       ");
+			groupGrafo.setLayout(new RowLayout(SWT.VERTICAL));
+				new Button(groupGrafo, 0).setText("Componentes Conexas");
+				btnCiclo = new Button(groupGrafo, SWT.NONE);
+				btnCiclo.setText("Ciclo");
+				btnCiclo.addListener(SWT.Selection, new Listener() {
+					public void handleEvent(Event event) {
+						byte[][] mtx;
+						mtx = graph.getMatriz();
+						mtx[4][3] += 1;
+						mtx[3][5] += 1;
+						mtx[5][4] += 1;
+						
+						replot(mtx);
+						
+					}
+				});
+				new Button(groupGrafo, 0).setText("Vértice de corte");
+				new Button(groupGrafo, 0).setText("Aresta de corte");
+				new Button(groupGrafo, 0).setText("Bipartição");
+				
+			
+			Group groupDigrafo = new Group(fakeComposite, SWT.SHADOW_IN);
+			groupDigrafo.setText("        Algoritmos Dígrafo       ");
+			groupDigrafo.setLayout(new RowLayout(SWT.VERTICAL));
+				new Button(groupDigrafo, 0).setText("Fecho Transitivo");
+				new Button(groupDigrafo, 0).setText("Ordenação Topológica");
+				new Button(groupDigrafo, 0).setText("Componentes Fortemente Conexas");
+
+				
+			
+			graphComposite = new Composite(shell, SWT.EMBEDDED);
+			//graphComposite.setLayout(new GridLayout());
+			graphComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+			//graphComposite.setSize(500, 350);
+			
+			
+			graphFrame = SWT_AWT.new_Frame(graphComposite);
+			
+				
+			
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	private void replot(byte[][] mtx)
+	{
+		Graph graphtmp = new Graph();
+		graphtmp.setMatriz(mtx);
+		graphtmp.setNlc(graph.getNlc());
+				
+		try {
+			parser.GraphToXml(graphtmp, "resultado.xml");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-//		controle.setArquivo(fc.getSelectedFile().getAbsolutePath());
-//		IO fluxo = new IO(controle.getArquivo());
+		graphFrame.remove(panel);
+		
+		panel = new java.awt.Panel(new java.awt.BorderLayout());						
+	    panel.add(GraphView.demo("resultado.xml", "name"));
+	    
+	   	    
+	    graphFrame.add(panel);
+	    graphFrame.repaint();
+	    graphFrame.setVisible(true);
+		
+	}
+	
+	private void openFile()
+	{
+
+		try {
+			FileDialog dlg = new FileDialog(shell, SWT.OPEN);
+			dlg.setFilterNames(new String[] {"txt", "Todos os arquivos (*.*)" });
+			dlg.setFilterExtensions(new String[] {"*.txt", "*.*" });
+			dlg.setText("Abrir Arquivo...");
+			fileName = dlg.open();
+			if (fileName != null) {
+
+				String infile = "config.xml";
+				String label = "name";
+				
+				graph = new Graph();
+				
+				io = new IOFile(fileName);
+				io.Read();
+				System.out.println("uhet1");
+				parser = new Parser(io.getV(), io.getN(), graph);
+				System.out.println("uhet2");
+				
+				System.out.println("uhet1");
+				parser.GraphToXml(graph, infile);
+				System.out.println("uhet2");
+
+				if (panel != null){
+					graphFrame.remove(panel);
+					panel = null;
+				}
+				
+				panel = new java.awt.Panel(new java.awt.BorderLayout());
+			    //panel.add(GraphViewEdgeDecoratorV2.demo(infile, "name", graphComposite.getSize().x, graphComposite.getSize().y));
+			    panel.add(GraphView.demo(infile, label));
+			    panel.setVisible(true);
+			    graphFrame.add(panel);
+			    graphFrame.setVisible(true);
+ 			    
+			} else
+				System.out.println("Cancelar");
+		} catch (Exception e){
+			MessageBox messageBox = new MessageBox(shell, SWT.ICON_ERROR);
+			messageBox.setMessage("Erro ao abrir o arquivo" );
+		}
+	}
+
+	/**
+	 * @brief Método Principal que chama a tela de ínicio.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		Display display = Display.getDefault();
+		Shell shell = new Shell(display);
+		shell.setText("Trabalho de Análise de Algoritmos - Grafos");
+		shell.setMaximized(true);
+		Main inst = new Main(shell, SWT.NULL);
+		
+		GridLayout layout = new GridLayout(2,false);
+		
+		shell.setLayout(layout);
+		shell.layout();
+		shell.open();
+		
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+
 	}
 
 }
